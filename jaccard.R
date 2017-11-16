6a7ead1efe8ba78da679c221b39e5d52d6283c03
Boróka Pápay <papayboroka@gmail.com>
2017-11-16 14:32:59 +0100

    Add files via upload

---
+++ jaccard.R
@@ -0,0 +1,12 @@
+jaccard <- function(matrix1,matrix2){
+  shared_ties <- matrix1 * matrix2
+  diff_ties <- (matrix1 + matrix2) == 1
+  
+  sum_diff_ties = sum(diff_ties)
+  sum_shared_ties = sum(shared_ties)
+  divisor = sum_diff_ties + sum_shared_ties
+  jaccard_index = ifelse(divisor == 0, 
+                         1, 
+                         sum_shared_ties/divisor)
+  return(jaccard_index)
+}
